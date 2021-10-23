08768496556c1a40f811862ed8fde7f64e98ee58
tcyrus <tcyrus@users.noreply.github.com>
2021-10-23 22:56:11 +0000

    deploy: 09e97e362c885b3b9de534bd0b70805eb4e68166

---
+++ _nuxt/static/1635029770/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Farchive","\u002Fmascots","\u002Farchive\u002Fanthology","\u002Fmascots\u002Fakari","\u002Fmascots\u002Ffukaya","\u002Fmascots\u002Fshion","\u002Farchive\u002F2021\u002Fmascot_concept_contest"]})
