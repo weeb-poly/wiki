734626d0302ef0cbc871b83f979c596ddc9cdd44
tcyrus <tcyrus@users.noreply.github.com>
2021-10-23 22:51:44 +0000

    deploy: 6355ecbbc60abaefff243fad9cecb972d5b41c5d

--- _nuxt/static/1629691381/archive/anthology/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1629691381",layout:"default",error:a,serverRendered:true,routePath:"\u002Farchive\u002Fanthology",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1050a450"}}}}(null));
